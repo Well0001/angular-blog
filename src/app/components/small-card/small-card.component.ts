6bb333403721e3e246a718cb79d27532dedefb7d
wellington-luiz <wellingtonluiz010203@gmail.com>
2024-01-30 07:30:08 -0300

    content created

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -11,6 +11,9 @@ export class SmallCardComponent implements OnInit {
   photoLink:string = ""
   @Input()
   cardTitle:string = ""
+  @Input()
+  id:string = "0"
+
   constructor() { }
 
   ngOnInit(): void {
---
+++ src/app/data/datafake.ts
@@ -0,0 +1,21 @@
+export const dataFake = [{
+  "id": "1",
+  "photo": "https://vidafullstack.com.br/wp-content/uploads/2023/12/angular-1080x675.jpg" ,
+  "title": "Novidades na versão 17 do Framework",
+  "description": "A comunidade Angular recebeu com entusiasmo a tão aguardada versão 17, que transformou significativamente o framework, ampliando o foco em desempenho e na experiência da pessoa desenvolvedora,o que levou essa atualização a ser conhecida como 'Angular Renaissance', ou seja, Renascimento Angular. Vamos conhecer o Angular 17 e como ele busca otimizar a experiência do desenvolvimento em Angular?"
+},
+{
+  "id":"2",
+  "photo": "https://medicinasa.com.br/wp-content/uploads/2023/08/digital-robotica-inteligencia-artificial-inovacao2d.jpg" ,
+  "title": "IA generativa democratizada",
+  "description": `A Inteligência Artificial Generativa, ou GenAI, é um subconjunto da Inteligência Artificial capaz de criar, gerar ou produzir conteúdos, como texto, imagens ou até mesmo código de programação automaticamente.
+
+  Ela é treinada a partir de grandes conjuntos de dados para compreender e replicar as informações, processando padrões mais complexos do que o Machine Learning.
+
+  Essa capacidade irá beneficiar empresas de inúmeras, a começar por:
+
+  Democratizar o conhecimento, uma vez que as aplicações baseadas em GEnAI podem tornar as informações mais acessíveis;
+  Automatizar a criação de conteúdo, já que um modelo generativo pode pegar o que aprendeu com os exemplos que lhe foram mostrados e criar algo do zero, totalmente novo.`
+}
+]
+
