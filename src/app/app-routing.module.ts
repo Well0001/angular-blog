8ba0c084870dba3515d521a8daa927d046c4361f
wellington-luiz <wellingtonluiz010203@gmail.com>
2024-01-30 08:11:16 -0300

    router configured

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { HomeComponent } from './pages/home/home.component';
 import { ContentComponent } from './pages/content/content.component';
 
 const routes: Routes = [
-  {path:'', component:HomeComponent},
-  {path:'content/:id', component:ContentComponent}
+  {path:'', component:HomeComponent, pathMatch:'full'},
+  {path:'content/:id', component:ContentComponent},
+  {path:'**', redirectTo:''}
 ];
 
 @NgModule({
